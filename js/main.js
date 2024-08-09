6b9a013d301bbc8283e1447243ac90d25ea111c4
mountainboy01 <dharmendrachauhan977@gmail.com>
2024-08-09 18:31:16 +0530

    fix

--- js/main.js
+++ js/main.js
@@ -6,12 +6,24 @@ toggleIcon.addEventListener("click",()=>{
    nav.classList.toggle("active");
    toggleIcon.classList.toggle("fa-xmark");
 });
-
-
-
-
-
-
-
-
 /*************************NAVBAR**************************/
+
+
+/***************************** SCROLL TOP JS **********************************/
+const scrollTop = document.querySelector("#scrollTop");
+window.addEventListener("scroll",()=>{
+	if(window.pageYOffset>300){
+	scrollTop.classList.add("active");
+     }
+
+     else{
+     	scrollTop.classList.remove("active");
+     }
+})
+
+scrollTop.addEventListener("click",()=>{
+	window.scrollTo(0 ,0);
+})
+
+
+/***************************** SCROLL TOP JS **********************************/
