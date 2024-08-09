ba6711f255d210074f117d49036fd87e1c82883b
mountainboy01 <dharmendrachauhan977@gmail.com>
2024-08-09 13:40:45 +0530

    real-state

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*************************NAVBAR**************************/
+const nav = document.querySelector("#nav");
+const toggleIcon = document.querySelector("#toggleIcon");
+
+toggleIcon.addEventListener("click",()=>{
+   nav.classList.toggle("active");
+   toggleIcon.classList.toggle("fa-xmark");
+});
+
+
+
+
+
+
+
+
+/*************************NAVBAR**************************/
